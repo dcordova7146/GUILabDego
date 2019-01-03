616f96395cc67d57bd63db72c96e3298cd6b5e42
dego <dcordova7146@bths.edu>
2019-01-03 10:14:29 -0500

    day 2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,27 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.layout.VBox;
+
 import java.net.URL;
+import java.util.Timer;
 
 public class Main extends Application {
+    int count = 0;
+    private int timeleft = 1000;
+    boolean x = false;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -22,13 +32,31 @@ public class Main extends Application {
         stageTest.setHeight(500);
         stageTest.setX(650);
         stageTest.setY(300);
-        stageTest.setTitle("Test");
+        stageTest.setTitle("FAST CLICKER");
         stageTest.initStyle(StageStyle.UTILITY);
         stageTest.show();
         VBox vbox = loader.<VBox>load();
         Scene testScene = new Scene(vbox);
         stageTest.setScene(testScene);
         testScene.setCursor(Cursor.CROSSHAIR);
+
+        Button clicker = new Button();
+        Text curScore = new Text("Score: 0");
+        clicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                count++;
+                curScore.setText("Score: " + count);
+            }
+        });
+       AnimationTimer time = new AnimationTimer() {
+
+           @Override
+           public void handle(long now) {
+
+           }
+       };time.start();
+
 
 
     }
